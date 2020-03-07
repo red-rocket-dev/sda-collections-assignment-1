b74583a059bbb47daeafc7f30ee1a7927532f963
Jakub <kubplon@gmail.com>
2020-03-07 01:14:53 +0100

    Set,List assignment

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        1. Utworz liste (dowolna implementacja) przechowujaca String
+        2. Dodaj do niej elementy: Ania, Kasia, Ania, Tomek, Michal, Kasia, Paweł, Zbych
+        3. Wstaw pomiedzy Anie i Tomka Klaudię (policz, który to indeks i w to miejsce wstaw imię)
+           - pomocna bedzie metoda lista.add(<indeks pod ktorym wstawic>, <co wstawic>
+        4. Przenieś elementy do innej kolekcji, która nie przechowuje duplikatów
+           - przydatna będzie metoda addAll z tej "innej kolekcji"
+        5. Usuń element Ania
+        6. Wypisz tylko elementy, których ostatnia litera to "a"
+         */
+    }
+}
