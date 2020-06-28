cf0207b8ca000663855346dd0a966330bb9f73b3
Jakub <kubplon@gmail.com>
2020-06-28 09:30:30 +0200

    solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +12,44 @@ public class Main {
         5. Usuń element Ania
         6. Wypisz tylko elementy, których ostatnia litera to "a"
          */
+        List<String> names = new LinkedList<>();
+
+        names.add("Ania");
+        names.add("Kasia");
+        names.add("Ania");
+        names.add("Tomek");
+        names.add("Michal");
+        names.add("Kasia");
+        names.add("Paweł");
+        names.add("Zbych");
+
+        //names.add(3, "Klaudia");
+        //albo tak (indexOf nie mozemy uzyc, bo tylko znalazloby wystapienie Tomka albo Ani, nie ich oboje)
+        ListIterator<String> namesListIterator = names.listIterator();
+        String previous = null;
+        String next;
+        while (namesListIterator.hasNext()) {
+            next = namesListIterator.next();
+            if("Ania".equals(previous) && "Tomek".equals(next) ) {
+                namesListIterator.previous();
+                namesListIterator.add("Klaudia");
+                break;
+            }
+            previous = next;
+        }
+
+
+        System.out.println(names);
+        Set<String> uniqueNames = new HashSet<>(names);
+        //uniqueNames.addAll(names); <- tak też się da, ale lepiej użyć konstruktora
+        System.out.println(uniqueNames);
+        uniqueNames.remove("Ania");
+        System.out.println(uniqueNames);
+
+        for (String name : uniqueNames) {
+            if (name.endsWith("a")) {
+                System.out.println(name);
+            }
+        }
     }
 }
